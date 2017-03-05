f66bd112224e98140de7a0e00e3c766688376722
Austin Dill <adill7@gatech.edu>
2017-03-05 12:19:50 -0500

    Skeleton for MacroScanner init

---
+++ WindowsApplication4/MacroScanner.vb
@@ -0,0 +1,13 @@
+﻿Public Class MacroScanner
+
+    Dim tokens As LinkedList(Of String)
+
+    Public Sub scan(buffer As String)
+
+        'Loop through input string
+        For Each c As Char In buffer
+
+        Next
+
+    End Sub
+End Class
---
+++ WindowsApplication4/Token.vb
@@ -0,0 +1,28 @@
+﻿Public Class Token
+
+    Dim type As TokenType
+    Dim str As String
+
+    Public Sub New(type As TokenType, str As String)
+        Me.type = type
+        Me.str = str
+    End Sub
+
+    Public Enum TokenType
+        ForBegin
+        Dots
+        ForLoop
+        EndFor
+        Park
+        Slew
+        SlewTo
+        LParen
+        RParen
+        Semi
+        Record
+        StopRecord
+        Int
+        Float
+    End Enum
+
+End Class
