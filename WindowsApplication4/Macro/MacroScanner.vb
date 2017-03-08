beb2f41d89c991cec34854b6dd0eaef25405c77a
Austin Dill <adill7@gatech.edu>
2017-03-07 19:20:11 -0500

    Scanner up and running! Onto the parser

--- WindowsApplication4/Macro/MacroScanner.vb
+++ WindowsApplication4/Macro/MacroScanner.vb
@@ -27,30 +27,54 @@
                     newToken = New Token(Token.TokenType.Colon, ":")
                     tokens.AddLast(newToken)
                 Case " "
+                Case vbCr
+                Case vbTab
+                Case vbCrLf
+                Case vbLf
                 Case Else
                     currToken = currToken + curr
                     Select Case currToken
                         Case "park"
                             newToken = New Token(Token.TokenType.Park, "park")
                             tokens.AddLast(newToken)
+                            currToken = ""
                         Case "slew"
                             newToken = New Token(Token.TokenType.Slew, "slew")
                             tokens.AddLast(newToken)
+                            currToken = ""
                         Case "record"
                             newToken = New Token(Token.TokenType.Record, "record")
                             tokens.AddLast(newToken)
+                            currToken = ""
                         Case "stop"
                             newToken = New Token(Token.TokenType.StopRecord, "stop")
                             tokens.AddLast(newToken)
+                            currToken = ""
                         Case "begin"
                             newToken = New Token(Token.TokenType.Begin, "begin")
                             tokens.AddLast(newToken)
+                            currToken = ""
                         Case "for"
                             newToken = New Token(Token.TokenType.ForBegin, "for")
                             tokens.AddLast(newToken)
+                            currToken = ""
                         Case "loop"
                             newToken = New Token(Token.TokenType.ForLoop, "loop")
                             tokens.AddLast(newToken)
+                            currToken = ""
+                        Case Else
+                            If IsNumeric(currToken) And Not IsNumeric(lookAhead) Then
+                                If lookAhead <> "." Then
+                                    If currToken.Contains(".") Then
+                                        newToken = New Token(Token.TokenType.Float, currToken)
+                                        currToken = ""
+                                    Else
+                                        newToken = New Token(Token.TokenType.Int, currToken)
+                                        currToken = ""
+                                    End If
+                                    tokens.AddLast(newToken)
+                                End If
+                            End If
                     End Select
             End Select
         Next
